6cd8c012e86746402a22fa3254d28d9b50a2a3e5
Gary Theilman <gtheilman@gmail.com>
2014-11-16 18:05:13 -0600

    Added Mathjax

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,35 @@
 'use strict';
 
+MathJax.Hub.Config({skipStartupTypeset: true});
+MathJax.Hub.Configured();
+
+
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
+    .directive("mathjaxBind", function () {
+        return {
+            restrict: "A",
+            controller: ["$scope", "$element", "$attrs", function ($scope, $element, $attrs) {
+                $scope.$watch($attrs.mathjaxBind, function (value) {
+                    $element.text(value == undefined ? "" : value);
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
+                });
+            }]
+        };
+    })
+    .controller('View2Ctrl', function ($scope) {
 
-.controller('View2Ctrl', [function() {
+        var firstnumber = 21;
+        $scope.CKDEPI = "\\[    GFR \\approx ClCr \\approx" + firstnumber + "\\times SrCr^{-1.154}\\times Age^{-0.203}\\times [1.210 \\; if \\; Black] \\times [0.742 \\; if \\; female]     \\]";
+        $scope.child = "\\[    GFR \\approx ClCr \\approx  \\frac{k\\times Height}{SrCr}    \\]";
 
-}]);
+    });
+
+
+
