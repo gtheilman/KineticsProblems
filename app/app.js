3ddc43eb8cedde4ba38916a3919cbfff3624ea4e
Gary Theilman <gtheilman@gmail.com>
2014-11-20 17:55:52 -0600

    Got chart tests working

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
-  'ui.bootstrap'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version',
+    'ui.bootstrap',
+    'nvd3ChartDirectives'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
